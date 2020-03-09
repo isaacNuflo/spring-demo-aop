259914fcec6981d840cce65277c18a162bdfb0a6
isaacNuflo <isaac.nuflo21@gmail.com>
2020-03-08 23:16:40 -0500

    AOP Spring

---
+++ src/com/luv2code/aopdemo/MainDemoApp.java
@@ -0,0 +1,23 @@
+package com.luv2code.aopdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.luv2code.aopdemo.dao.AccountDAO;
+import com.luv2code.aopdemo.dao.MembershipDAO;
+
+public class MainDemoApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DemoConfig.class);
+		
+		AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
+		
+		theAccountDAO.addAccount(new Account(), true);
+		theAccountDAO.setName("account");
+		theAccountDAO.setServiceCode("1");
+		
+		context.close();
+	}
+
+}

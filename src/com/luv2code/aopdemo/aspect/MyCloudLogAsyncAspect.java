f729fc495c0c4ea7c7ec67a5838a205564a2d04f
isaacNuflo <isaac.nuflo21@gmail.com>
2020-03-09 23:03:19 -0500

    order annotation

--- src/com/luv2code/aopdemo/aspect/MyCloudLogAsyncAspect.java
+++ src/com/luv2code/aopdemo/aspect/MyCloudLogAsyncAspect.java
@@ -2,19 +2,17 @@ package com.luv2code.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
-@Order(2)
-public class MyDemoLoggingAspect {
+@Order(1)
+public class MyCloudLogAsyncAspect {
 
-	// @Before("execution(public void add*())")
 	@Before("com.luv2code.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
-	public void beforeAddAccountAdvice() {
-		System.out.println("====> Executing @Before advice on addAccount()");
+	public void logToCloudAsync() {
+		System.out.println("====> Logging to Cloud in async fashion");
 	}
 
 }
